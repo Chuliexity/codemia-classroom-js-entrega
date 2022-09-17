40f398996a92508b28a816f93ac709ba0b40b8aa
chuliexity <julietad2105@gmail.com>
2022-09-17 14:52:34 -0300

    entrega clase 5 ejercicio 3

---
+++ class-5/ejercicio-3.js
@@ -0,0 +1,17 @@
+const str = "Mepgustapcomerpmilanesa"
+
+let split = str.split("p")
+console.log(split)
+
+const str2 = "Todas las mañanas canto rap"
+let substring = str2.substring(10,16)
+console.log(substring)
+
+const str3 = "Mañana es un día muy Importante, porque voy a ver Palomas a la Plaza"
+let upperCase = str3.toUpperCase()
+let lowerCase = str3.toLowerCase()
+console.log(upperCase,lowerCase)
+
+const str4 = "Me gusta muchisimo el deporte"
+let replace = str4.replace("deporte", "sol")
+console.log(replace)
